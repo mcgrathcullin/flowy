de8cad4e20bd4e4a38fec993ecadf6b38f1fb2de
CJ <49197069+mcgrathcullin@users.noreply.github.com>
2024-03-22 04:26:38 -0500

    Update panzoom.js

--- panzoom.js
+++ panzoom.js
@@ -1,23 +1,35 @@
 let transformX = 0;
 let transformY = 0;
 let scale = 1;
+let lastPosX = 0;
+let lastPosY = 0;
+let isPanning = false;
 
 export function initPanZoom(svg) {
     const hammer = new Hammer(svg);
     hammer.get('pinch').set({ enable: true });
-    hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+    hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL, pointers: 2 });
 
     hammer.on('panstart', (event) => {
+        isPanning = true;
+        lastPosX = event.center.x;
+        lastPosY = event.center.y;
         svg.style.cursor = 'grabbing';
     });
 
     hammer.on('panmove', (event) => {
-        transformX += event.deltaX;
-        transformY += event.deltaY;
+        if (!isPanning) return;
+        const deltaX = event.center.x - lastPosX;
+        const deltaY = event.center.y - lastPosY;
+        lastPosX = event.center.x;
+        lastPosY = event.center.y;
+        transformX += deltaX;
+        transformY += deltaY;
         applyTransform(svg);
     });
 
     hammer.on('panend', (event) => {
+        isPanning = false;
         svg.style.cursor = 'grab';
     });
 
